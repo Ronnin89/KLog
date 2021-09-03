f2ba6967271bf1f3918b929baa892c9b9acbfe1d
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-09-03 13:49:53 -0400

    v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Grade_round } from "./components/Grades_round";
 
 export function App() {
-    return <Grade_round  grades={[38, 43, 99, 97, 29, 29, 0, 100, 40, 41, 42, 43, 44, 45, 37, 39]}/>;
+    return <Grade_round  grades={[38, 43, 99, 97]}/>;
 }
--- src/components/Grades_round.jsx
+++ src/components/Grades_round.jsx
@@ -1,42 +1,49 @@
 import React from "react";
 
 export function Grade_round({grades}) {
+    if (grades.count < 60 && grades.count > 0) {
+        return <h1> Limite de datos o no hay datos </h1>
+    }else{
+        let count = -1;
+        var grades_rounds = grades.map(function(grade){
+        count = count + 1;    
+            
+            if( grade >= 39 && ( grade % 10 === 9 || grade % 10 === 8 || grade % 10 === 4 || grade % 10 === 3)){
+            return <tr style= {{backgroundColor: getBackgroundColor(grade)}}>
+                        <td>
+                            { count }
+                        </td>
+                        <td>
+                            { grade }
+                        </td>
+                        <td>
+                            { Math.round(grade/5)*5 }
+                        </td>
+                    </tr>
+            
+        }
+        return <tr style= {{backgroundColor: getBackgroundColor(grade)}}>
+                        <td>
+                            { count }
+                        </td>
+                        <td>
+                            { grade }
+                        </td>
+                        <td>
+                            { grade }
+                        </td>
+                    </tr>
+        })
+        return <table >
+                    <tr>
+                        <th>ID</th>
+                        <th>Nota Original</th>
+                        <th>Nota Final</th>
+                    </tr>
+                    {grades_rounds}
+                </table>
     
-    var grades_rounds = grades.map(function(grade){
-        if( grade >= 39 && ( grade % 10 === 9 || grade % 10 === 8 || grade % 10 === 4 || grade % 10 === 3)){
-          return <tr style= {{backgroundColor: getBackgroundColor(grade)}}>
-                    <td>
-                        {grades.indexOf(grade)}
-                    </td>
-                    <td>
-                        { grade }
-                    </td>
-                    <td>
-                        { Math.round(grade/5)*5 }
-                    </td>
-                </tr>
-      }
-      return <tr style= {{backgroundColor: getBackgroundColor(grade)}}>
-                    <td>
-                        {grades.indexOf(grade)}
-                    </td>
-                    <td>
-                        { grade }
-                    </td>
-                    <td>
-                        { grade }
-                    </td>
-                </tr>
-    })
-    return <table >
-                <tr>
-                    <th>ID</th>
-                    <th>Nota Original</th>
-                    <th>Nota Final</th>
-                </tr>
-                {grades_rounds}
-            </table>
-
+    }
 }
 
 const getBackgroundColor = (grade) =>{
